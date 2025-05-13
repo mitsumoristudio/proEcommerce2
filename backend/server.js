9664341051f2c33287ad8ffe01ad29706c30b49e
satoshimitsumori <smitsumori@hotmail.com>
2025-05-13 10:06:18 -0500

    frontend build

--- backend/server.js
+++ backend/server.js
@@ -75,17 +75,17 @@ const __dirname = path.resolve(); // Set _dirname to current directory
 app.use(`/uploads`, express.static(path.join(__dirname, `../uploads`))); // changed the pathname because the root folder would not accept /uploads
 
 // Prepare for Production
-// if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === "production") {
     // set static folder
     app.use(express.static(path.join(__dirname, "/frontend/build")));
     // Any route that is not api will be redirected to index.html
     app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html")));
 
-// } else {
-//     app.get("/", (req, res) => {
-//         res.send("API is currently running ...")
-//     })
-// }
+} else {
+    app.get("/", (req, res) => {
+        res.send("API is currently running ...")
+    })
+}
 
 
 // Error Handler
